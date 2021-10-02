af43b1ecb5d8d93cb55eb53c66b4eda48393fb1b
arsalanhub <arsalanmohd237@gmail.com>
2021-10-02 17:47:27 +0530

    upper case initial phase

--- src/components/TextForm.js
+++ src/components/TextForm.js
@@ -1,14 +1,29 @@
 import React, { useState } from "react";
 
 export default function TextForm(props) {
+  const handleUpClick = () => {
+    console.log("Upper Case Clicked");
+    setText("You have clicked on handleupClick")
+  };
+  const handleOnChange = () => {
+    console.log("On Change");
+  };
   const [text, setText] = useState("Enter text here");
   return (
     <div>
       <h1>{props.heading}</h1>
       <div className="mb-3">
-        <textarea className="form-control" value={text} id="myBox" rows="8"></textarea>
+        <textarea
+          className="form-control"
+          value={text}
+          onChange={handleOnChange}
+          id="myBox"
+          rows="8"
+        ></textarea>
       </div>
-      <button className="btn btn-primary">Convert to Upper Case</button>
+      <button className="btn btn-primary" onClick={handleUpClick}>
+        Convert to Upper Case
+      </button>
     </div>
   );
 }
