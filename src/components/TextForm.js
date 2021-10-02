1cf47aff17c4cc61d73b8ed34d3add4e02ed9d4e
arsalanhub <arsalanmohd237@gmail.com>
2021-10-02 17:39:20 +0530

    added state

--- src/components/TextForm.js
+++ src/components/TextForm.js
@@ -1,15 +1,12 @@
-import React from "react";
+import React, { useState } from "react";
 
 export default function TextForm(props) {
+  const [text, setText] = useState("Enter text here");
   return (
     <div>
-      <h1>{props.heading}</h1>  
+      <h1>{props.heading}</h1>
       <div className="mb-3">
-        <textarea
-          className="form-control"
-          id="myBox"
-          rows="8"
-        ></textarea>
+        <textarea className="form-control" value={text} id="myBox" rows="8"></textarea>
       </div>
       <button className="btn btn-primary">Convert to Upper Case</button>
     </div>
