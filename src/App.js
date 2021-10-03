6af62591a4237d8f2a073520942cef4e2200b89e
arsalanhub <arsalanmohd237@gmail.com>
2021-10-03 12:46:05 +0530

    about section

--- src/App.js
+++ src/App.js
@@ -1,13 +1,15 @@
 import "./App.css";
+import About from "./components/About";
 import Navbar from "./components/Navbar";
-import TextForm from "./components/TextForm";
+// import TextForm from "./components/TextForm";
 
 function App() {
   return (
     <>
       <Navbar title="News Overload"/>
       <div className="container my-3">
-          <TextForm heading="Enter your text to analyze below"/>
+          {/* <TextForm heading="Enter your text to analyze below"/> */}
+          <About />
       </div>
     </>
   );
