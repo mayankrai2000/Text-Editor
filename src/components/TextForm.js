4a129b6e366acab1497faa6a4230425febc60712
arsalanhub <arsalanmohd237@gmail.com>
2021-10-03 10:51:44 +0530

    Preview feature and minutes read Preview feature lets us see what is present in text box on website. And minutes feature tells how much it will take to read the block

--- src/components/TextForm.js
+++ src/components/TextForm.js
@@ -3,12 +3,12 @@ import React, { useState } from "react";
 export default function TextForm(props) {
   const handleUpClickUpper = () => {
     let newText = text.toUpperCase();
-    setText(newText)
+    setText(newText);
   };
 
   const handleUpClickLower = () => {
     let newText = text.toLowerCase();
-    setText(newText)
+    setText(newText);
   };
 
   const handleOnChange = (event) => {
@@ -17,23 +17,32 @@ export default function TextForm(props) {
 
   const [text, setText] = useState("Enter text here");
   return (
-    <div>
-      <h1>{props.heading}</h1>
-      <div className="mb-3">
-        <textarea
-          className="form-control"
-          value={text}
-          onChange={handleOnChange}
-          id="myBox"
-          rows="8"
-        ></textarea>
+    <>
+      <div className="container">
+        <h1>{props.heading}</h1>
+        <div className="mb-3">
+          <textarea
+            className="form-control"
+            value={text}
+            onChange={handleOnChange}
+            id="myBox"
+            rows="8"
+          ></textarea>
+        </div>
+        <button className="btn btn-primary mx-3" onClick={handleUpClickUpper}>
+          Convert to Upper Case
+        </button>
+        <button className="btn btn-primary" onClick={handleUpClickLower}>
+          Convert to Lower Case
+        </button>
       </div>
-      <button className="btn btn-primary mx-3" onClick={handleUpClickUpper} >
-        Convert to Upper Case
-      </button>
-      <button className="btn btn-primary" onClick={handleUpClickLower}>
-        Convert to Lower Case
-      </button>
-    </div>
+      <div className="container my-3">
+          <h1>Your text summary</h1>
+          <p>{text.split(" ").length} words, {text.length} characters</p>
+          <p>{0.008*text.split(" ").length} Minutes Read</p>
+          <h2>Preview</h2>
+          <p>{text}</p>
+      </div>
+    </>
   );
 }
