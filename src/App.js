ab33e147dcec5505f0178ab1cda2cd7faf371d20
kaushal-sudo <kaushalgupta2103@gmail.com>
2021-10-10 19:48:23 +0530

    added alert for dark and light mode

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React, { useState } from "react";
 import "./App.css";
+import Alert from "./components/Alert";
 // import About from "./components/About";
 import Navbar from "./components/Navbar";
 import TextForm from "./components/TextForm";
@@ -9,6 +10,14 @@ function App() {
   const [mode, setMode] = useState("light");
   const [textMode, setTextMode] = useState("Enable Dark Mode");
   const [textColour, setTextColour] = useState("dark")
+  const [alert, setAlert] = useState(null);
+
+  const showAlert = (message, type) => {
+    setAlert({
+      msg: message,
+      type: type
+    })
+  }
 
   const toggleMode = () => {
     if (mode === "light") {
@@ -16,11 +25,13 @@ function App() {
       setTextMode("Enable Light Mode");
       setTextColour("light");
       document.body.style.backgroundColor = 'grey';
+      showAlert("Dark mode has been enabled", "success")
     } else {
       setMode("light");
       setTextMode("Enable Dark Mode");
       setTextColour("dark");
       document.body.style.backgroundColor = 'white';
+      showAlert("Light mode has been enabled", "success")
     }
   };
   return (
@@ -32,6 +43,7 @@ function App() {
         textMode={textMode}
         textColour={textColour}
       />
+      <Alert alert={alert} />
       <div className="container my-3">
         <TextForm heading="Enter your text to analyze below" mode={mode}/>
         {/* <About /> */}
