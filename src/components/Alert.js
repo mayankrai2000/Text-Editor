08a0f6665490f00a076e27ffb1ac76be8b97af21
arsalanhub <arsalanmohd237@gmail.com>
2021-10-11 17:29:20 +0530

    custom type added to alert

--- src/components/Alert.js
+++ src/components/Alert.js
@@ -3,7 +3,7 @@ import React from "react"
 function Alert(props) {
     return (
         // Below statement is equivalent to. if (props. alert != NULL)
-        props.alert && <div class="alert alert-warning alert-dismissible fade show" role="alert">
+        props.alert && <div class={`alert alert-${props.alert.type} alert-dismissible fade show`} role="alert">
         <strong>{props.alert.type}</strong>: {props.alert.msg}
         <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
         </div>
