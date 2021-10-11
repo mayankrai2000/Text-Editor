ca856a8c39c40f801f18bb80c82e25ee3d3e179c
arsalanhub <arsalanmohd237@gmail.com>
2021-10-11 18:12:53 +0530

    added setTimeout for alert

--- src/components/Alert.js
+++ src/components/Alert.js
@@ -9,7 +9,6 @@ function Alert(props) {
         // Below statement is equivalent to. if (props. alert != NULL)
         props.alert && <div class={`alert alert-${props.alert.type} alert-dismissible fade show`} role="alert">
         <strong>{capitalize(props.alert.type)}</strong>: {props.alert.msg}
-        <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
         </div>
     );
 }
